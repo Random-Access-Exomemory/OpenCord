740c3553fc777635523f016b6f98753aadf999b0
Random Access Exomemory <Random.Access.Exomemory@gmail.com>
2021-10-05 10:27:27 +0100

    Escape strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,17 +62,17 @@ android {
             dimension = "api"
             applicationIdSuffix = ".discord"
 
-            buildConfigField("String", "API_URL", "https://discord.com")
-            buildConfigField("String", "CDN_URL", "https://cdn.discordapp.com")
-            buildConfigField("String", "GATEWAY_URL", "wss://gateway.discord.gg")
+            buildConfigField("String", "API_URL", "\"https://discord.com\"")
+            buildConfigField("String", "CDN_URL", "\"https://cdn.discordapp.com\"")
+            buildConfigField("String", "GATEWAY_URL", "\"wss://gateway.discord.gg\"")
         }
         create("fosscord") {
             dimension = "api"
             applicationIdSuffix = ".fosscord"
 
-            buildConfigField("String", "API_URL", "https://dev.fosscord.com")
-            buildConfigField("String", "CDN_URL", "https://cdn.fosscord.com")
-            buildConfigField("String", "GATEWAY_URL", "wss://gateway.fosscord.com")
+            buildConfigField("String", "API_URL", "\"https://dev.fosscord.com\"")
+            buildConfigField("String", "CDN_URL", "\"https://cdn.fosscord.com\"")
+            buildConfigField("String", "GATEWAY_URL", "\"wss://gateway.fosscord.com\"")
         }
     }
 }
