2fe44dbfddc9acd2febc302e1a15fb59bb9c9205
Random Access Exomemory <Random.Access.Exomemory@gmail.com>
2021-10-05 10:17:39 +0100

    Remove const from API urls

--- app/src/main/java/com/xinto/opencord/network/util/Const.kt
+++ app/src/main/java/com/xinto/opencord/network/util/Const.kt
@@ -2,6 +2,6 @@ package com.xinto.opencord.network.util
 
 import com.xinto.opencord.BuildConfig
 
-const val discordApiUrl = "${BuildConfig.API_URL}/api/v9/"
-const val discordCdnUrl = "${BuildConfig.CDN_URL}"
-const val gatewayUrl = "${BuildConfig.GATEWAY_URL}/?v=9&encoding=json"
+val discordApiUrl = "${BuildConfig.API_URL}/api/v9/"
+val discordCdnUrl = "${BuildConfig.CDN_URL}"
+val gatewayUrl = "${BuildConfig.GATEWAY_URL}/?v=9&encoding=json"
